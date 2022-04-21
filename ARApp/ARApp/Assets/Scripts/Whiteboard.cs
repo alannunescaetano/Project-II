d6c81637a5aea6db68538aa2973d7d64793795af
Alan Nunes Caetano <alan.caetano3@gmail.com>
2022-04-21 10:57:52 +0100

    implementing AR whiteboard

--- ARApp/ARApp/Assets/Scripts/Whiteboard.cs
+++ ARApp/ARApp/Assets/Scripts/Whiteboard.cs
@@ -5,7 +5,7 @@ public class Whiteboard : MonoBehaviour
 {
     private TextMeshPro _text;
     private Syllable[] syllables;
-    private string[] strSyllables = new [] { "PA", "LA", "VRA" };
+    private string[] strSyllables = { "PA"};
     
     private int x = 0;
     
@@ -30,8 +30,8 @@ public class Whiteboard : MonoBehaviour
                 highlightedSyllableIndex = 0;
         }*/
         
-        //highlightSyllable(highlightedSyllableIndex);
-        highlightCharacters(0, 7);
+        highlightSyllable(highlightedSyllableIndex);
+        //highlightCharacters(0, 7);
     }
 
     private void highlightSyllable(int highlightedSyllableIndex)
@@ -81,7 +81,50 @@ public class Whiteboard : MonoBehaviour
             return;
         
         var info = _text.textInfo;
-        for (int charIndex = 0; charIndex < info.characterCount; charIndex++)
+        
+        int meshIndex = info.characterInfo[0].materialReferenceIndex;
+        int vertexIndex = _text.textInfo.characterInfo[0].vertexIndex;
+        
+        Color32[] vertexColors = info.meshInfo[meshIndex].colors32;
+        
+        if (vertexColors != null)
+        {
+            // print(charIndex + " " + info.characterInfo[charIndex].character + " " + color);
+            vertexColors[vertexIndex + 0] = highlightColor;
+            vertexColors[vertexIndex + 1] = highlightColor;
+            vertexColors[vertexIndex + 2] = highlightColor;
+            vertexColors[vertexIndex + 3] = highlightColor;
+        }
+        
+        meshIndex = info.characterInfo[1].materialReferenceIndex;
+        vertexIndex = _text.textInfo.characterInfo[1].vertexIndex;
+        
+        vertexColors = info.meshInfo[meshIndex].colors32;
+        
+        if (vertexColors != null)
+        {
+            // print(charIndex + " " + info.characterInfo[charIndex].character + " " + color);
+            vertexColors[vertexIndex + 0] = highlightColor;
+            vertexColors[vertexIndex + 1] = highlightColor;
+            vertexColors[vertexIndex + 2] = highlightColor;
+            vertexColors[vertexIndex + 3] = highlightColor;
+        }
+        
+        meshIndex = info.characterInfo[2].materialReferenceIndex;
+        vertexIndex = _text.textInfo.characterInfo[2].vertexIndex;
+        
+        vertexColors = info.meshInfo[meshIndex].colors32;
+        
+        if (vertexColors != null)
+        {
+            // print(charIndex + " " + info.characterInfo[charIndex].character + " " + color);
+            vertexColors[vertexIndex + 0] = initialColor;
+            vertexColors[vertexIndex + 1] = initialColor;
+            vertexColors[vertexIndex + 2] = initialColor;
+            vertexColors[vertexIndex + 3] = initialColor;
+        }
+        
+        /*for (int charIndex = 0; charIndex < info.characterCount; charIndex++)
         {
             int meshIndex = info.characterInfo[charIndex].materialReferenceIndex;
             int vertexIndex = _text.textInfo.characterInfo[charIndex].vertexIndex;
@@ -90,11 +133,29 @@ public class Whiteboard : MonoBehaviour
             if (vertexColors != null)
             {
                 Color color = charIndex >= startCharIndex && charIndex <= endCharIndex ? highlightColor : initialColor;
+                print(charIndex + " " + info.characterInfo[charIndex].character + " " + color);
                 vertexColors[vertexIndex + 0] = color;
                 vertexColors[vertexIndex + 1] = color;
                 vertexColors[vertexIndex + 2] = color;
                 vertexColors[vertexIndex + 3] = color;
             }
+        }*/
+        
+        _text.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
+    }
+    
+    private void highlightCharacters2(int startCharIndex, int endCharIndex)
+    {
+        Color initialColor = Color.black;
+        Color highlightColor = Color.green;
+
+        if (_text == null)
+            return;
+        
+        var info = _text.textInfo;
+        for (int charIndex = 0; charIndex < info.characterCount; charIndex++)
+        {
+            info.characterInfo[charIndex].material.color = Color.green;
         }
         
         _text.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
